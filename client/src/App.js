0b51361e71dbf182bb1aeb88f12ae0f3bc7bf0c3
Frankenstein <88062514+maxwillner@users.noreply.github.com>
2022-01-03 18:38:16 -0500

    implemented Apollo client; created client mutations and query files

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
+
 function App() {
   return (
-    <Router>
+    <ApolloProvider client={client}>
+      <Router>
       <>
         <Navbar />
         <Switch>
@@ -15,7 +27,9 @@ function App() {
           <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
         </Switch>
       </>
-    </Router>
+      </Router>
+    </ApolloProvider>
+
   );
 }
 
